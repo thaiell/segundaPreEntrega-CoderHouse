a1018b2574dd4defce33aed20200649b5e737671
Thaiel <thaielog@gmail.com>
2023-03-08 00:14:08 -0300

    entrega

--- main.js
+++ main.js
@@ -1,205 +1,190 @@
-/*                                  FUNCIONES                                             */
-/* 
-function bienvenido(){
-
-    let eleccion = parseInt(prompt(`Busque en nuestro catalogo sus productos favoritos: 1.BIJOUTERIE / 2.VITROFUSION / 3.TALLADOSDEMADERA / 4.SALIR`));
-    
-    while(eleccion != 4){
-        if (eleccion === 1){
-            let eleccionProducto = parseInt(prompt(`BIJOUTERIE: ${catalogoBijouterie}`));
-            switch(eleccionProducto){
-                case 1:
-                    alert("Usted ha elegido ANILLOS");
-                    break;
-                case 2:
-                    alert("Usted ha elegido un COLGANTE");
-                    break;
-                case 3:
-                    alert("Usted ha elegido COLLARES");
-                    break;
-                case 4:
-                    alert("Usted ha elegido un PULSERAS");
-                    break;
-                case 5:
-                    alert("Usted ha elegido AROS");
-                    break;
-                case 6:
-                    alert("Usted ha elegido PENDIENTES");
-                    break;
-                case 9: 
-                    bienvenido();
-                    break;
-                case 0:
-                    eleccion = 4;
-                    break;
-                default:
-                    alert("Ha habido un problema, vuelva a intentar");
-                    break;
-                }
-        
-        
-            } else if (eleccion === 2){
-                let eleccionProducto = parseInt(prompt(`VITROFUSION: ${catalogoVitroFusion}`));
-                switch(eleccionProducto){
-                    case 1:
-                        alert("Usted ha elegido BANDEJAS");
-                        break;
-                    case 2:
-                        alert("Usted ha elegido un PLATOS");
-                        break;
-                    case 3:
-                        alert("Usted ha elegido ADORNOS");
-                        break;
-                    case 9: 
-                        bienvenido();
-                        break;
-                    case 0:
-                        eleccion = 4;
-                        break;
-                    default:
-                        alert("Ha habido un problema, vuelva a intentar");
-                        break;
-                }
-            } else if (eleccion === 3){
-                let eleccionProducto = parseInt(prompt(`TALLADOS EN MADERA: ${catalogoTallados}`));
-                switch(eleccionProducto){
-                    case 1:
-                        alert(`Usted a elegido un CUADRO`);
-                        break;
-                    case 2:
-                        alert(`Usted a elegido una ESCULTURA`);
-                        break;
-                    case 3:
-                        alert(`Usted a elegido CUBIERTOS`);
-                        break;
-                    case 4:
-                        alert(`Usted a elegido un PLATOS`);
-                        break;
-                    case 5:
-                        alert(`Usted a elegido un TABLAS`);
-                        break;
-                    case 6:
-                        alert(`Usted a elegido un VASIJAS`);
-                        break;
-                    case 9:
-                        bienvenido();
-                        break;
-                    case 0:
-                        eleccion = 4;
-                        break;
-                    default:
-                        alert("Ha habido un problema, vuelva a intentar");
-                        break;
-                    }
-            }
-         }
-    } 
-    
-    let bijouteriePrecio = 750;
-    let vitroFusionPrecio = 1000;
-    let talladoAMano = 850;
-    
-    
-    function calcularPrecio(productoPrincipal){
-    if (productoPrincipal == "1"){
-        let precioTotal = bijouteriePrecio + 500;
-        alert(precioTotal);
-    } else if (productoPrincipal == "2"){
-        let precioTotal = vitroFusionPrecio + 500;
-        alert(precioTotal);
-    } else if (productoPrincipal == "3"){
-        let precioTotal = talladoAMano + 500;
-        alert(precioTotal);
-    } else {
-        alert("Disculpamos las molestias pero algo ha salido mal, por favor vuelva a intentar")
-        let productoPrincipal = prompt(`${persona} le ayudaremos a saber cuanto va a ser su monto final con envio incluido, que tipo de producto compro: 1.BIJOUTERIE / 2.VITROFUSION / 3.TALLADO A MANO / 9.NADA`);
-        calcularPrecio(productoPrincipal);
-    }
-    }
-
-//                                                      INICIO DE LOS ALGORITMOS                                                    
-// pide nombre y apellido
-
-let persona = prompt("Ingrese su nombre y apellido para que sea registrado en nuestro sistema");
-
-while (persona === ""){ 
-    persona = prompt("Por favor, debe ingresar un nombre valido. Intente nuevamente");
-}
-alert(`Se ha registrado como ${persona}`);
-
-
-//mensaje bienvenida
-
-const alertMessage = `Hola ${persona}!, te damos la bienvenida a "Tu Manufactureo", donde podras encontrar todo tipo de articulos y accesorios 100% hechos a mano por artesanos locales`;
-alert(alertMessage);
-
-
-let propositoDeCompra = "Indiquenos con que motivo realizara su compra: 1.REGALO / 2.USO PROPIO";
-let catalogoBijouterie = "1.ANILLOS / 2.COLGANTES / 3.COLLARES / 4.PULSERAS / 5.AROS / 6.PENDIENTES / 9.VOLVER / 0.SALIR";
-let catalogoVitroFusion = "1.BANDEJAS / 2.PLATOS / 3.ADORNOS / 9.VOLVER / 0.SALIR";
-let catalogoTallados = "1.CUADROS / 2.ESCULTURAS / 3.CUBIERTOS / 4.PLATOS / 5.TABLAS / 6. VASIJAS / 9.VOLVER / 0.SALIR";
-
-bienvenido();
-
-let productoPrincipal = prompt(`${persona} le ayudaremos a saber cuanto va a ser su monto final con envio incluido, que tipo de producto compro: 1.BIJOUTERIE / 2.VITROFUSION / 3.TALLADO A MANO / 9.NADA`);
-
-calcularPrecio(productoPrincipal); */
-
-
 // Array Usuarios
 
 const usuarios = [
-{id: 1, nombre: "Maria", apellido: "Ruiz", edad: 25, tel: 634563},
-{id: 2, nombre: "Javier", apellido: "Mariscal", edad: 35, tel: 123456},
-{id: 3,  nombre: "Octavio", apellido: "Timtin", edad: 18, tel: 645645},
-{id: 4, nombre: "Mariano", apellido: "Josefiscal", edad: 50, tel: 865785 },
-]
+    {id: 1, nombre: "Maria", apellido: "Ruiz", edad: 25, tel: 634563, dni: 12312312},
+    {id: 2, nombre: "Javier", apellido: "Mariscal", edad: 35, tel: 123456, dni: 23435345},
+    {id: 3,  nombre: "Octavio", apellido: "Timtin", edad: 18, tel: 645645, dni: 1231234523},
+    {id: 4, nombre: "Mariano", apellido: "Josefiscal", edad: 50, tel: 865785, dni: 4441231},
+    ]
 
-/* 
-let nombre = prompt("Ingresa tu nombre");
-let apellido = prompt("Ingresa tu apellido");
-let email = prompt("Ingresa tu email");
-let tel = prompt("Ingresa tu numero de telefono sin 15"); 
-*/
-
-class UsuarioConstructor{
-    constructor(id, nombre, apellido, edad, tel){
-        this.id = usuarios.length + 1;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.edad = edad;
-        this.tel = tel;
+    class UsuarioConstructor{
+        constructor(nombre, apellido, edad, tel, dni){
+            this.id = usuarios.length + 1;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+            this.tel = tel;
+            this.dni = dni;
     }
-    }
-/* 
-    let nombre = prompt("Ingresa tu nombre");
-    let apellido = prompt("Ingresa tu apellido");
-    let edad = prompt("Ingresa tu edad")
-    let tel = prompt("Ingresa tu numero de telefono"); 
-    
-    usuarios.push(new UsuarioConstructor(id, nombre, apellido, edad, tel));
-*/
-    
-    // const id = usuarios.length + 1;
-
-    usuarios.push(new UsuarioConstructor(UsuarioConstructor.id, "Thaiel", "Ojalvo", 17, 21560306));
-    console.log(usuarios);
-
-
-/* reemplazarNombre = (nombre) => {
-    let index = nombre.indexOf(nombre);
-
-    if(index != -1){
-        usuarios
-    }
-
-
 }
-const eliminar = (nombre) => {
-    let index = nombre.indexOf(nombre);
+        const usuariosClientes = [];
 
-    if(index != -1){
-        nombres.splice(index, 1);
-        console.log(nombres)
+    const stock = [
+        {id: 1, nombreProducto: "collar", precioProducto: 1500, stockTotal: 10},
+        {id: 2, nombreProducto: "aretes", precioProducto: 2000, stockTotal: 5},  
+        {id: 3, nombreProducto: "pulsera", precioProducto: 1000, stockTotal: 6},
+        {id: 4, nombreProducto: "tobillera", precioProducto: 900, stockTotal: 2},
+        {id: 5, nombreProducto: "anillos", precioProducto: 1250, stockTotal: 8},
+        {id: 6, nombreProducto: "pendientes", precioProducto: 1350, stockTotal: 100},
+    ];
+    const carrito = [];
+    const nombresStock = [];
+    
+    stock.forEach(item => nombresStock.push(item.nombreProducto));
+ 
+
+
+         class Cliente {
+        constructor(dniUsuarioBuscado){
+            this.nombre = dniUsuarioBuscado.nombre;
+            this.apellido = dniUsuarioBuscado.apellido;
+            this.edad = dniUsuarioBuscado.edad;
+            this.tel = dniUsuarioBuscado.tel;
+            this.dni = dniUsuarioBuscado.dni;
+        } 
+    } 
+    class Producto {
+        constructor(nombreProducto, precioProducto, stockTotal){
+            this.idProducto = stock.length + 1;
+            this.nombreProducto = nombreProducto;
+            this.precioProducto = precioProducto;
+            this.stockTotal = stockTotal;
+        } 
     }
-} */
+    
+    verificarOAgregarProducto = () => {
+        let buscadorNombre = prompt(`Que producto desea verificar de su registro: / ${concatenados}`);
+        let productoEncontrado = stock.find(item => item.nombreProducto === buscadorNombre);
+    
+        if (productoEncontrado){
+            alert("El producto ya existe en la base de datos")
+            let sumarStockPregunta = prompt("Prefiere sumarle cantidad al stock? Responder con *si* o *no*");
+            if(sumarStockPregunta.toLowerCase() == "si"){
+                let sumarStockCantidad = parseInt(prompt("Cuantas unidades de stock desea agregar?"));
+                productoEncontrado.stockTotal += sumarStockCantidad;
+                    let mensaje = 
+                    `
+                    ID: ${productoEncontrado.id}
+                    Nombre: ${productoEncontrado.nombreProducto}
+                    Precio: ${productoEncontrado.precioProducto}
+                    Stock: ${productoEncontrado.stockTotal}
+                    `
+                    alert(mensaje);
+            
+            }
+        }
+        else {
+            let siONo = prompt("El producto no esta en nuestra base de datos, quieres agregarla?");
+                if (siONo.toLowerCase() === "si"){
+                    let nombreProducto = prompt("Ingrese el nombre del nuevo producto");
+                     let precioProducto = parseInt(prompt("Ingrese el precio del nuevo producto"));
+                     let stockTotal = parseInt(prompt("Ingrese la cantidad de stock del nuevo producto"));  
+                  
+                     stock.push(new Producto(nombreProducto, precioProducto, stockTotal));
+                } 
+                
+                else {
+                    alert("Operacion Cancelada")
+                }
+            }
+    }
+aumentarStock = () => {
+    let sumarStock = prompt("Que producto quieres agregarle stock? " + concatenados);
+    let producto = stock.find(i => i.nombreProducto === sumarStock);
+    let cantidad = parseInt(prompt("Cuanta cantidad le quiere agregar?"));
+        producto.stockTotal = producto.stockTotal + cantidad;
+        alert(`A ${sumarStock} se le añadieron ${cantidad} quedando con un total de ${producto.stockTotal}`);
+}
+    
+    /* OPERACIONES CLIENTES */
+comprar = () => {
+    let productoAComprar = prompt("Que producto desea comprar? / " + concatenados);
+        
+        productoAComprarEncontrado = stock.find(item => item.nombreProducto === productoAComprar);
+    
+    if (productoAComprarEncontrado){
+            
+            let dniUsuario = prompt("Ingrese su numero de DNI para su correcto reconocimiento");
+            let dniUsuarioBuscado = usuarios.find(item => item.dni === dniUsuario);
+    
+            alert(`Usted es ${dniUsuarioBuscado.nombre} ${dniUsuarioBuscado.apellido} y oficialmente es un cliente comprando su primer producto; un ${productoAComprar} :) Bienvenido`);
+    
+            productoAComprarEncontrado.stockTotal -= 1; 
+    
+            usuariosClientes.push(new Cliente(dniUsuarioBuscado));
+            console.log(usuariosClientes);
+    }
+}
+    
+verStock = () => {
+    alert(concatenados);
+}
+
+saludarAlDueño = () => {
+    alert("Le enviaremos tus saludos a Thaiel :)")
+}
+
+filtrado = (menor) => {
+        let filtrado = stock.filter(item => item.precioProducto >= menor);
+        filtrado.forEach((item) => {
+            let mensaje = `
+        Nombre: ${item.nombreProducto}
+        Precio: ${item.precioProducto}
+        `
+        alert(mensaje);
+        })
+}
+
+
+    
+    /*              INICIO PROGRAMA                 */
+    
+    const concatenados = nombresStock.join(" / ");
+    
+    
+    
+     let identificador = prompt("Quien eres? Cliente o Administrador?");
+    
+    if(identificador.toLowerCase() === "administrador"){
+
+        let operacion = prompt("Que operacion va a realizar?: 1. VERIFICAR O AGREGAR PRODUCTO - 2. AUMENTAR STOCK");
+        switch (operacion.toLowerCase()){
+            case "1":
+                verificarOAgregarProducto();
+                break;
+            case "2":
+                aumentarStock();
+                break;
+            default:
+            alert("Operacion cancelada")
+            break;
+        }
+    } else if(identificador.toLowerCase() === "cliente"){
+        let nombre = prompt("Ingresa tu nombre");
+        let apellido = prompt("Ingresa tu apellido");
+        let edad = prompt("Ingresa tu edad")
+        let tel = prompt("Ingresa tu numero de telefono"); 
+        let dni = prompt("Ingrese su DNI para reconocerlo automaticamente");
+        
+        usuarios.push(new UsuarioConstructor(nombre, apellido, edad, tel, dni)); 
+                
+        let operacion = prompt("Que operacion va a realizar?: 1. COMPRAR - 2. VER STOCK - 3. SALUDAR AL DUEÑO - 4. BUSCAR PRODUCTOS MEDIANTE PRECIO");
+        switch(operacion.toLowerCase()){
+            case "1":
+                comprar();
+                break;
+            case "2":
+                verStock();
+                break;
+            case "3":
+                saludarAlDueño();
+                break;
+            case "4": 
+                let menor = prompt("Ingrese el monto minimo para hacer el filtrado");
+                filtrado(menor);
+            break;
+            default: 
+                alert("Operacion cancelada");
+                break;
+        }
+    }
